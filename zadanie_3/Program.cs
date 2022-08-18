c74380506ea512ed96ed0943b007e913db126d9d
Roman Burmistrov <110119637+roamb@users.noreply.github.com>
2022-08-18 21:20:21 +0300

    Update Program.cs

--- zadanie_3/Program.cs
+++ zadanie_3/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-for (int i = 1; i <= number; i++)
+int num = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= num; i++)
 {
     Console.WriteLine( Math.Pow(i,3));
 }
