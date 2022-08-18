b4c68a43f64f263e5c09aeec0229d2128a910cde
roan b <roman7tati@gmail.com>
2022-08-18 21:18:55 +0300

    Home Lesson #3

---
+++ zadanie_1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+string num = Console.ReadLine();
+if (num[0] == num[4] && num[1] == num[3])        
+    Console.WriteLine("Число является палиндромом");
+else
+{
+    Console.WriteLine("Число не является палиндромом");
+}
